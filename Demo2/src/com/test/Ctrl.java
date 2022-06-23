5560e46368fa404e7e20a12aa7bd7a9774b48aa5
lvzhenying <alvzhenying@163.com>
2022-06-23 14:26:17 +0800

    那雀食

---
+++ Demo2/src/com/test/Ctrl.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ctrl
+ */
+public class Ctrl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Ctrl() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		 String json = "{code:11,msg:'返回成功'}";
+		 response.getWriter().print(json);
+		 response.getWriter().flush();
+		 response.getWriter().close();
+		 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
